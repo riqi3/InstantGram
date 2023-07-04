42485b0e1f86d06067b849ee3f858c17e90930dc
riqi3 <enrique.pacudan@usjr.edu.ph>
2023-07-04 12:38:13 +0800

    google sign up

--- instantgram/lib/authentication/LoginScreen.dart
+++ instantgram/lib/authentication/LoginScreen.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
 
+import 'GoogleAuth.dart';
+
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            width: 200,
+            height: 200,
+            child: GoogleAuth(),
+            ),
+        ],
+      ),
+    );
   }
 }
