b29e9659fde801f2049161884a14398eb3571d60
riqi3 <enrique.pacudan@usjr.edu.ph>
2023-07-17 16:27:25 +0800

    create post and homepage updates

--- instantgram/lib/views/homepage/homeview.dart
+++ instantgram/lib/views/homepage/homeview.dart
@@ -3,33 +3,17 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-<<<<<<< HEAD:instantgram/lib/views/homepage/homeview.dart
-import 'package:instantgram/state/auth/provider/auth_state_provider.dart';
-import 'package:instantgram/state/img_upload/model/ftype.dart';
-import 'package:instantgram/state/img_upload/service/image_picker_service.dart';
-import 'package:instantgram/state/post_prefference/provider/post_pref_provider.dart';
-import 'package:instantgram/views/Tabviews/my_post.dart';
-import 'package:instantgram/views/Tabviews/newsfeed.dart';
-import 'package:instantgram/views/Tabviews/search.dart';
 import 'package:instantgram/views/components/PromptDialogs/dialog_model.dart';
-import 'package:instantgram/views/components/PromptDialogs/logout_prompt.dart';
-import 'package:instantgram/views/createpost/create_post_view.dart';
-=======
-import 'package:instantgram/state/auth/providers/auth_state_provider.dart';
 
-import 'package:instantgram/state/image_upload/models/file_type.dart';
-
-import 'package:instantgram/views/components/dialogs/alert_dialog_model.dart';
-import 'package:instantgram/views/components/dialogs/logout_dialog.dart';
-import 'package:instantgram/views/constants/strings.dart';
-
-import '../../state/image_upload/helper/image_picker_helper.dart';
-import '../../state/post_settings/post_settings_provider.dart';
-import '../create_new_post_view.dart';
-import '../tabs/home_view.dart';
-import '../tabs/search_view.dart';
-import '../tabs/user_posts_view.dart';
->>>>>>> talandron-additions-1:instantgram/lib/views/main/main_view.dart
+import '../../state/auth/provider/auth_state_provider.dart';
+import '../../state/img_upload/model/ftype.dart';
+import '../../state/img_upload/service/image_picker_service.dart';
+import '../../state/post_prefference/provider/post_pref_provider.dart';
+import '../Tabviews/my_post.dart';
+import '../Tabviews/newsfeed.dart';
+import '../Tabviews/search.dart';
+import '../components/PromptDialogs/logout_prompt.dart';
+import '../createpost/create_post_view.dart';
 
 class MainView extends ConsumerStatefulWidget {
   const MainView({Key? key}) : super(key: key);
@@ -45,38 +29,21 @@ class _MainViewState extends ConsumerState<MainView> {
       length: 3,
       child: Scaffold(
         appBar: AppBar(
-<<<<<<< HEAD:instantgram/lib/views/homepage/homeview.dart
           title: const Text('Instant-gram'),
-=======
-          title: const Text(
-            Strings.appName,
-          ),
->>>>>>> talandron-additions-1:instantgram/lib/views/main/main_view.dart
           actions: [
             IconButton(
               icon: const FaIcon(
                 FontAwesomeIcons.film,
               ),
               onPressed: () async {
-<<<<<<< HEAD:instantgram/lib/views/homepage/homeview.dart
-=======
-                // pick a video first
->>>>>>> talandron-additions-1:instantgram/lib/views/main/main_view.dart
                 final videoFile =
                     await ImagePickerHelper.pickVideoFromGallery();
                 if (videoFile == null) {
                   return;
                 }
 
-<<<<<<< HEAD:instantgram/lib/views/homepage/homeview.dart
                 ref.refresh(postSettingProvider);
 
-=======
-                // reset the postSettingProvider
-                ref.refresh(postSettingProvider);
-
-                // go to the screen to create a new post
->>>>>>> talandron-additions-1:instantgram/lib/views/main/main_view.dart
                 if (!mounted) {
                   return;
                 }
@@ -84,11 +51,7 @@ class _MainViewState extends ConsumerState<MainView> {
                   context,
                   MaterialPageRoute(
                     builder: (_) => CreateNewPostView(
-<<<<<<< HEAD:instantgram/lib/views/homepage/homeview.dart
                       fileType: FType.video,
-=======
-                      fileType: FileType.video,
->>>>>>> talandron-additions-1:instantgram/lib/views/main/main_view.dart
                       fileToPost: videoFile,
                     ),
                   ),
@@ -97,25 +60,14 @@ class _MainViewState extends ConsumerState<MainView> {
             ),
             IconButton(
               onPressed: () async {
-<<<<<<< HEAD:instantgram/lib/views/homepage/homeview.dart
-=======
-                // pick an image first
->>>>>>> talandron-additions-1:instantgram/lib/views/main/main_view.dart
                 final imageFile =
                     await ImagePickerHelper.pickImageFromGallery();
                 if (imageFile == null) {
                   return;
                 }
 
-<<<<<<< HEAD:instantgram/lib/views/homepage/homeview.dart
                 ref.refresh(postSettingProvider);
 
-=======
-                // reset the postSettingProvider
-                ref.refresh(postSettingProvider);
-
-                // go to the screen to create a new post
->>>>>>> talandron-additions-1:instantgram/lib/views/main/main_view.dart
                 if (!mounted) {
                   return;
                 }
@@ -123,11 +75,7 @@ class _MainViewState extends ConsumerState<MainView> {
                   context,
                   MaterialPageRoute(
                     builder: (_) => CreateNewPostView(
-<<<<<<< HEAD:instantgram/lib/views/homepage/homeview.dart
                       fileType: FType.image,
-=======
-                      fileType: FileType.image,
->>>>>>> talandron-additions-1:instantgram/lib/views/main/main_view.dart
                       fileToPost: imageFile,
                     ),
                   ),
@@ -140,11 +88,7 @@ class _MainViewState extends ConsumerState<MainView> {
             IconButton(
               onPressed: () async {
                 final shouldLogOut =
-<<<<<<< HEAD:instantgram/lib/views/homepage/homeview.dart
                     await const LogoutPrompt().present(context).then(
-=======
-                    await const LogoutDialog().present(context).then(
->>>>>>> talandron-additions-1:instantgram/lib/views/main/main_view.dart
                           (value) => value ?? false,
                         );
                 if (shouldLogOut) {
